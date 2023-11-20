37b0ceb76f570b9415012b121c219e1712fd7248
Jan McSorley <mcsorleyjan@gmail.com>
2023-11-19 21:54:32 -0600

    Build component structure

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,56 @@
 import './App.css';
+import Header from '../Header/Header';
+import Headlines from '../Headlines/Headlines';
+import SelectedArticle from '../SelectedArticle/SelectedArticle';
+import Error from '../Error/Error';
+// **import { getAllArticles } from '../../apiCalls';
+import { mockData } from '../../mockData/mockData';
+import { Routes, Route } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [allArticles, setAllArticles] = useState(mockData);
+  const [error, setError] = useState('');
+  // const [selectedArticle, setSelectedArticle] = useState(0);
+
+  // **useEffect(() => {
+  //   const getAsyncArticles = async () => {
+  //     try {
+  //       const articles = await getAllArticles()
+  //       setAllArticles(articles)
+  //     } catch (error) {
+  //       setError(`${error.message}: Something went wrong. Please try again.`)
+  //     }
+  //   }
+  //   getAsyncArticles()
+  // }, [])
+
+  // const handleSelection = () => {
+
+  // }
+
   return (
-   <p>Hello from APP</p>
+  <>
+   <Header />
+   {error ? (
+    <Error error={error} />
+   ) : (
+   <main>
+    <Routes>
+      <Route
+        path="/"
+        element={<Headlines allArticles={allArticles}/>}
+      />
+      <Route
+        path="/articleDetails/:id"
+        element={<SelectedArticle />}
+      />
+      <Route
+        path="*" element={<Error error={error}/>}
+      />
+    </Routes>
+   </main> )}
+  </>
   );
 }
 
---
+++ src/components/Error/Error.js
@@ -0,0 +1,9 @@
+import './Error.css';
+
+function Error() {
+    return (
+        <p>This is an error message.</p>
+    )
+}
+
+export default Error;
