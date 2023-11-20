f34b908fe4a29651c9e98a58f07d738b297355ed
Jan McSorley <mcsorleyjan@gmail.com>
2023-11-19 23:39:49 -0600

    Add date to HeadlineCard

--- src/components/HeadlineCard/HeadlineCard.js
+++ src/components/HeadlineCard/HeadlineCard.js
@@ -1,17 +1,26 @@
 import './HeadlineCard.css';
 import { Link } from 'react-router-dom';
 
-function HeadlineCard( { headline, content, description, url, imageURL, datePublished, id} ) {
-    
-    return (
-        <div className="card" key={id} id={id}>
-            <h2>{headline}</h2>
-            <img src={imageURL} alt="" />
-            <p>{description}</p>
-            <Link to={`/details/${id}`}>Read More <span className="sr-only">about {headline}</span></Link>
-        </div>
-
-    )
+function HeadlineCard({
+  headline,
+  content,
+  description,
+  url,
+  imageURL,
+  datePublished,
+  id,
+}) {
+  return (
+    <div className="card" key={id} id={id}>
+      <h2>{headline}</h2>
+      <p>Date Published: {datePublished}</p>
+      <img src={imageURL} alt="" />
+      <p>{description}</p>
+      <Link to={`/details/${id}`}>
+        Read More <span className="sr-only">about {headline}</span>
+      </Link>
+    </div>
+  );
 }
 
 export default HeadlineCard;
--- src/components/Headlines/Headlines.js
+++ src/components/Headlines/Headlines.js
@@ -1,6 +1,18 @@
 import './Headlines.css';
 import HeadlineCard from '../HeadlineCard/HeadlineCard';
 
+function formatUSDate(dateString) {
+    const dateObject = new Date(dateString);
+    if (!isNaN(dateObject.getTime())) {
+        const formattedDateString = 
+        (dateObject.getMonth() +1).toString().padStart(2, '0') + '/' +
+        dateObject.getDate().toString().padStart(2, '0') + '/' +
+        dateObject.getFullYear();
+        return formattedDateString;
+    }
+    return 'No date available.';
+}
+
 function Headlines( {allArticles} ) {
     const headlineCard = allArticles.articles.map((headline, index) => {
         return <HeadlineCard
@@ -9,7 +21,7 @@ function Headlines( {allArticles} ) {
             description={headline.description}
             url={headline.url}
             imageURL={headline.urlToImage}
-            datePublished={headline.publishedAt}
+            datePublished={formatUSDate(headline.publishedAt)}
             key={index + 1}
             id={index + 1}
         />
